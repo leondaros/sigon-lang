1abf7a64199ea5e63e38affacccae01978721bc2
Leon Daros <leondaros@gmail.com>
2019-10-01 15:20:20 -0300

    Creation of Sensors, Actuators and ontologic context

--- src/main/java/br/ufsc/ine/agent/context/ontologic/OntologicContextService.java
+++ src/main/java/br/ufsc/ine/agent/context/ontologic/OntologicContextService.java
@@ -1,11 +1,41 @@
 package br.ufsc.ine.agent.context.ontologic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.ufsc.ine.agent.context.custom.CustomContext;
+import br.ufsc.ine.agent.context.ontologic.semanticWeb.SparqlObject;
+import br.ufsc.ine.agent.context.ontologic.semanticWeb.SparqlResult;
+import br.ufsc.ine.agent.context.ontologic.semanticWeb.SparqlSearch;
 
 public class OntologicContextService extends CustomContext{
 	
+	SparqlSearch sparqlSearch = new SparqlSearch();
+	List<SparqlObject> sparqlObjects = new ArrayList<SparqlObject>();
+		
 	public OntologicContextService(String name) {
 		super(name);
+	}
+	
+	public void clearObjectsList(){
+		sparqlObjects.clear();
+	}
+	
+	public void addObjectToList(String subject,String predicate, String object){
+		SparqlObject triple = new SparqlObject();
+		triple.setUri(subject);
+		triple.setPredicate(predicate);
+		triple.setObject(object);
+		sparqlObjects.add(triple);
+	}
+	
+	public List<SparqlResult> searchNewURI(List<SparqlObject> objects){
+		clearObjectsList();
+		return sparqlSearch.searchDbpedia(objects);
+	}
+	
+	public List<SparqlResult> searchKnownURI(List<SparqlObject> objects){
+		return null;
 	}
 	
 }
---
+++ src/main/java/br/ufsc/ine/agent/context/ontologic/actuators/BuildQuestion.java
@@ -0,0 +1,14 @@
+package br.ufsc.ine.agent.context.ontologic.actuators;
+
+import java.util.List;
+
+import br.ufsc.ine.agent.context.communication.Actuator;
+
+public class BuildQuestion extends Actuator{
+
+	@Override
+	public void act(List<String> args) {
+		
+	}
+
+}
